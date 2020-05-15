baa184d0ac75a6ac057f278441972fe166999161
nightblade9 <nightbladecodes@gmail.com>
2020-05-15 02:13:32 -0400

    Basic tile component

--- prototypes/01-battle-cooldowns/main.js
+++ prototypes/01-battle-cooldowns/main.js
@@ -1,11 +1,20 @@
 Game = {
     // This defines our grid's size and the size of each of its tiles
     view: {
-    width: 800,
-    height: 400,
+        width: 960,
+        height: 576,
     },
 
     start: function() {
         Crafty.init(Game.view.width, Game.view.height);
+        Crafty.background('#004400');
+
+        for (var y = 0; y < Game.view.height / 32; y++)
+        {
+            for (var x = 0; x < Game.view.width / 32; x++)
+            {
+                var t = Crafty.e("Tile").initialize(x, y);
+            }
+        }
     }
 }
---
+++ prototypes/01-battle-cooldowns/map.js
@@ -0,0 +1,15 @@
+Crafty.c('Tile', {
+
+    init: function() {
+        this.requires("Actor");
+        return this;
+    },
+
+    initialize: function(x, y) {
+        this.tileX = x;
+        this.tileY = y;
+        this.move(this.tileX * 32, this.tileY * 32);
+        this.color("#008800");
+        return this;
+    }
+});
