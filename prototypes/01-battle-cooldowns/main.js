848e9868d96f7d67ef04ce9223a1f98291b108c8
nightblade9 <nightbladecodes@gmail.com>
2020-05-15 02:02:57 -0400

    Create main JS file

---
+++ prototypes/01-battle-cooldowns/main.js
@@ -0,0 +1,11 @@
+Game = {
+    // This defines our grid's size and the size of each of its tiles
+    view: {
+    width: 800,
+    height: 400,
+    },
+
+    start: function() {
+        Crafty.init(Game.view.width, Game.view.height);
+    }
+}
